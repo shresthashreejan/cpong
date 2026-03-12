c71581955594c1baeffe700e8f87326f63e0f9bd
shreejan <shr.shreejan@gmail.com>
2026-03-12 20:37:06 +1030

    Clean up

--- src/main.c
+++ src/main.c
@@ -1,54 +1,31 @@
-/*
-Raylib example file.
-This is an example main file for a simple raylib project.
-Use this as a starting point or replace it with your code.
-
-by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit https://creativecommons.org/publicdomain/zero/1.0/
-
-*/
-
 #include "raylib.h"
 
-#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+#include "resource_dir.h"
 
 int main ()
 {
-	// Tell the window to use vsync and work on high DPI displays
-	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+	// SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
-	// Create the window and OpenGL context
-	InitWindow(1280, 800, "Hello Raylib");
+	const int screenWidth = 1280;
+	const int screenHeight = 800;
 
-	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
-	SearchAndSetResourceDir("resources");
+	InitWindow(screenWidth, screenHeight, "Input keys");
+	// SearchAndSetResourceDir("assets");
 
-	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
-	
-	// game loop
-	while (!WindowShouldClose())		// run the loop until the user presses ESCAPE or presses the Close button on the window
+	Vector2 ballPosition = { (float)screenWidth/2, (float)screenHeight/2};
+
+	while (!WindowShouldClose())
 	{
-		// drawing
+		if (IsKeyDown(KEY_RIGHT)) ballPosition.x += 0.2f;
+		if (IsKeyDown(KEY_LEFT)) ballPosition.x -= 0.2f;
+		if (IsKeyDown(KEY_UP)) ballPosition.y -= 0.2f;
+		if (IsKeyDown(KEY_DOWN)) ballPosition.y += 0.2f;
 		BeginDrawing();
-
-		// Setup the back buffer for drawing (clear color and depth buffers)
 		ClearBackground(BLACK);
-
-		// draw some text using the default font
-		DrawText("Hello Raylib", 200,200,20,WHITE);
-
-		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
-		
-		// end the frame and get ready for the next one  (display frame, poll input, etc...)
+		DrawCircleV(ballPosition, 50, WHITE);
+		DrawFPS(10, 10);
 		EndDrawing();
 	}
-
-	// cleanup
-	// unload our texture so it can be cleaned up
-	UnloadTexture(wabbit);
-
-	// destroy the window and cleanup the OpenGL context
 	CloseWindow();
 	return 0;
 }
