86f36654d2142252cb4a019593de6a7a15ee98a9
shreejan <shr.shreejan@gmail.com>
2026-03-13 23:00:57 +1030

    Added constants.h

---
+++ include/constants.h
@@ -0,0 +1,11 @@
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+// Window
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+
+// Game
+#define BALL_BASE_SPEED 500.0f
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,28 +1,35 @@
-#include "raylib.h"
+#include <stdio.h>
 
+#include "raylib.h"
 #include "resource_dir.h"
+#include "constants.h"
 
 int main ()
 {
-	// SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
-
-	const int screenWidth = 1280;
-	const int screenHeight = 800;
-
-	InitWindow(screenWidth, screenHeight, "Input keys");
+	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Raylib");
 	// SearchAndSetResourceDir("assets");
+	SetTargetFPS(120);
 
-	Vector2 ballPosition = { (float)screenWidth/2, (float)screenHeight/2};
+	Vector2 ballPosition = { (float)SCREEN_WIDTH/2, (float)SCREEN_HEIGHT/2};
 
 	while (!WindowShouldClose())
 	{
-		if (IsKeyDown(KEY_RIGHT)) ballPosition.x += 0.2f;
-		if (IsKeyDown(KEY_LEFT)) ballPosition.x -= 0.2f;
-		if (IsKeyDown(KEY_UP)) ballPosition.y -= 0.2f;
-		if (IsKeyDown(KEY_DOWN)) ballPosition.y += 0.2f;
+		float dt = GetFrameTime();
+		float ballSpeed = BALL_BASE_SPEED * dt;
+
+		Vector2 direction = {0};
+
+		if (IsKeyDown(KEY_RIGHT)) direction.x += 1;
+		if (IsKeyDown(KEY_LEFT)) direction.x -= 1;
+		if (IsKeyDown(KEY_UP)) direction.y -= 1;
+		if (IsKeyDown(KEY_DOWN)) direction.y += 1;
+
+		ballPosition.x += direction.x * ballSpeed;
+		ballPosition.y += direction.y * ballSpeed;
+
 		BeginDrawing();
 		ClearBackground(BLACK);
-		DrawCircleV(ballPosition, 50, WHITE);
+		DrawCircleV(ballPosition, 50, GRAY);
 		DrawFPS(10, 10);
 		EndDrawing();
 	}
